ff37a7625c53bb5a956495bd223d621cb24bb473
wonhyoek <epahsejtmxm@naver.com>
2020-09-26 12:39:46 +0900

    Set Mongo DB

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb+srv://wonhyoek:ab8975@cluster0.jls2e.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send("Hello World"));
 
