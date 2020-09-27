4d37d56b49bc4ce7a933f4a8bcf236b4462739c6
wonhyoek <47857304+wonhyoek@users.noreply.github.com>
2020-09-27 16:11:51 +0900

    Login Page and Register Page Form

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './LoginPage.css';
 
 export default () => {
-    return (
-        <div>
-            LoginPage
+    
+      return (
+        <div className = 'login-box'>
+
+            <h1>Login</h1>
+
+            <div className = 'textbox'>
+              <input type = 'text' placeholder = 'Email'/>
+            </div>
+
+            <div className = 'textbox'>
+              <input type = 'password' placeholder = 'Password'/>
+            </div>
+
+            <input className = 'btn' type = 'button' value = 'Sign In'/>
+
         </div>
-    )
+      );
 }
--- client/src/components/views/RegisterPage/RegisterPage.js
+++ client/src/components/views/RegisterPage/RegisterPage.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import './RegisterPage.css';
 
 export default () => {
     return (
-        <div>
-            RegisterPage
+        <div className = 'register-box'>
+
+            <h1>Register</h1>
+
+            <div className = 'textbox'>
+              <input type = 'text' placeholder = 'Name'/>
+            </div>
+            
+            <div className = 'textbox'>
+              <input type = 'text' placeholder = 'Email'/>
+            </div>
+
+            <div className = 'textbox'>
+              <input type = 'password' placeholder = 'Password'/>
+            </div>
+
+            <div className = 'textbox'>
+              <input type = 'password' placeholder = 'Confirm Password'/>
+            </div>
+
+            <input className = 'btn' type = 'button' value = 'Sign Up'/>
+
         </div>
     )
 }
